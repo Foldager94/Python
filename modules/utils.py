d2af01dd6af6d7ab43472d25db91add452e87e14
Foldager94 <c.k.foldager@gmail.com>
2021-02-17 00:47:58 +0100

    week2

--- modules/utils.py
+++ modules/utils.py
@@ -1,4 +1,5 @@
-from os import listdir
+import os
+import re
 
 def get_file_names(folderpath,out="output.txt"):
     list = listdir(folderpath)
@@ -6,22 +7,40 @@ def get_file_names(folderpath,out="output.txt"):
         for l in list:
             o.write(l + " ")
         print(listdir(folderpath))
-           # o.write(listdir(folderpath))
     print('File is ready: ', out)
+    
     """ takes a path to a folder and writes all filenames in the folder to a specified output file"""
 
 def get_all_file_names(folderpath,out="output.txt"):
-    pass
+    for root, dirs, files in os.walk(folderpath):
+        for file in files:
+            with open(out, "a") as o:
+                    o.write(file + "\n")
+    
     """takes a path to a folder and write all filenames recursively (files of all sub folders to)"""
 
 def print_line_one(file_names):
-    pass
+    for file in file_names:
+        with open(file, "r") as o:
+            print(o.readline())
+            
     """takes a list of filenames and print the first line of each"""
 
 def print_emails(file_names):
-    pass
+    for file in file_names:
+        with open(file, 'r') as f:
+            for line in f:
+                if re.search(r'[\w\.-]+@[\w\.-]+', line):
+                    print(line)
+                
     """takes a list of filenames and print each line that contains an email (just look for @)"""
 
 def write_headlines(md_files, out="output.txt"):
-    pass
+    for file in md_files:
+        with open(file, 'r') as f:
+            for line in f:
+                if re.search("^#", line):
+                    with open(out, 'a') as o:
+                        o.write(line + "\n")
+
     """takes a list of md files and writes all headlines (lines starting with #) to a file"""
